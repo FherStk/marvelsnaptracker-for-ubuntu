537cbbfc0359fa5d7e3bb533537745abee73dc4a
FherStk <porrino.fernando@gmail.com>
2023-01-04 14:09:59 +0100

    space

--- marvel-snap-deck-tracker-ubuntu.sh
+++ marvel-snap-deck-tracker-ubuntu.sh
@@ -12,7 +12,6 @@ echo -e "${YELLOW}Copyright © 2023:${NC} Fernando Porrino Serrano"
 echo -e "${YELLOW}Under the AGPL license:${NC} https://github.com/FherStk/marvelsnaptracker-forubuntu/blob/main/LICENSE"
 echo
 echo -e "${PURPLE}Attention please:${NC} This is an Ubuntu binary builder for the Marvel Snap Deck Tracker by ${LCYAN}Razviar${NC}, please visit ${LCYAN}https://github.com/Razviar/marvelsnaptracker${NC} for further information."
-echo
 
 trap 'abort' 0
 
