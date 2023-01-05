a69804908ee907a8d30349a99c452d0422543b4d
FherStk <porrino.fernando@gmail.com>
2023-01-05 11:04:08 +0100

    ready

--- marvel-snap-deck-tracker-ubuntu.sh
+++ marvel-snap-deck-tracker-ubuntu.sh
@@ -26,49 +26,49 @@ fi
 
 trap 'abort' 0
 
-# #Update if new versions  
-# auto-update true
+#Update if new versions  
+auto-update true
 
-# echo ""
-# title "Updating apt sources:"
-# apt update
+echo ""
+title "Updating apt sources:"
+apt update
 
-# apt-install lxc
+apt-install lxc
 
-# echo ""
-# title "Setting up the LXC/LXD container:"    
-# if [ $(lxc storage list | grep -c "CREATED") -eq 0 ];
-# then    
-#     echo "Initializing the LXC/LXD container..."
-#     lxd init --auto
-# else
-#     echo "LXC/LXD container already initialized, skipping..."
-# fi
+echo ""
+title "Setting up the LXC/LXD container:"    
+if [ $(lxc storage list | grep -c "CREATED") -eq 0 ];
+then    
+    echo "Initializing the LXC/LXD container..."
+    lxd init --auto
+else
+    echo "LXC/LXD container already initialized, skipping..."
+fi
 
-# container="marvel-snap-deck-tracker-builder"
-# if [ $(lxc list | grep -c "$container") -eq 0 ];
-# then    
-#     lxc launch ubuntu:22.04 $container
-# else
-#     echo "LXC/LXD image already exists, skipping..."
-# fi
+container="marvel-snap-deck-tracker-builder"
+if [ $(lxc list | grep -c "$container") -eq 0 ];
+then    
+    lxc launch ubuntu:22.04 $container
+else
+    echo "LXC/LXD image already exists, skipping..."
+fi
 
-# echo
-# title "Building the binary:"
-# echo "Copying the build script within the container..."
-# lxc file push --recursive $SCRIPT_PATH ${container}/etc/
+echo
+title "Building the binary:"
+echo "Copying the build script within the container..."
+lxc file push --recursive $SCRIPT_PATH ${container}/etc/
 
-# echo "Running the build script within the container..."
-# lxc exec $container -- /bin/bash /etc/marvelsnaptracker-for-ubuntu/utils/build.sh
+echo "Running the build script within the container..."
+lxc exec $container -- /bin/bash /etc/marvelsnaptracker-for-ubuntu/utils/build.sh
 
-# echo
-# echo "Copying the binary to the local host..."
-# lxc file pull $container/root/marvelsnaptracker/out/'Marvel Snap Tracker-linux-x64' . --recursive
+echo
+echo "Copying the binary to the local host..."
+lxc file pull $container/root/marvelsnaptracker/out/'Marvel Snap Tracker-linux-x64' . --recursive
 
-# echo "Setting up permissions..."
-# rm -rf build
-# mv Marvel\ Snap\ Tracker-linux-x64/ build
-# chown -R $SUDO_USER:$SUDO_USER build 
+echo "Setting up permissions..."
+rm -rf build
+mv Marvel\ Snap\ Tracker-linux-x64/ build
+chown -R $SUDO_USER:$SUDO_USER build 
 
 echo
 question "Do you want to install the application? " "[Y/n]"
@@ -101,12 +101,16 @@ else
     cp -Rf build/* $input
 
     echo "Creating a shortcut into the app list..."    
-    src="marvel-snap-tracker.desktop"
-    dst="/home/$SUDO_USER/.local/share/applications/$src"    
+    dst="utils/marvel-snap-tracker.desktop"
+    src="${dst}.template"
 
-    cp -f utils/$src $dst
-    sed -i "s|'<install_path>'|'$input'|g" $dst   
-    chown -R $SUDO_USER:$SUDO_USER $dst
+    cp -f $src $dst
+    echo "PATH: $input"
+
+    sed -i "s|<INSTALL_PATH>|$input|g" $dst
+    chown -R $SUDO_USER:$SUDO_USER $dst  
+    run-in-user-session xdg-desktop-menu install $dst
+    rm $dst
 fi
 
 done-ok
